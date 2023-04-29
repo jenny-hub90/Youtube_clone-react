1f9bfabfb845a8c454ea06160d6e3ceb6e7fecd5
jenny-hub90 <jumbledjenny19@gmail.com>
2023-04-29 18:53:07 +0545

    made the searchbar functional

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { useParams } from 'react-router-dom';
+
+import { fetchFormAPI } from '../utils/fetchFormAPI';
+import {  Videos } from './';
 
 const SearchFeed = () => {
+  const [data,setData] = useState([]);
+  const { searchTerm } = useParams();
+
+  const fetchData = async ()=>{
+   const fetcheddata= await fetchFormAPI(`search?part=snippet&q=${searchTerm}`)
+    setData(fetcheddata.items)
+  }
+
+   useEffect(() =>  {
+    fetchData()
+  }, [searchTerm]);
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{overflowY:'auto', height: '90vh', flex : 2}}>
+        <Typography variant="h4"
+         fontWeight="bold" mb={2} sx={{
+          color:'black'}}>
+           Search Results for: <span style={{color: '#F31503'}}>
+            {searchTerm}
+            </span>
+            videos  
+        </Typography>
+
+        <Videos videos={data} />
+      </Box>
   )
 }
 
-export default SearchFeed
+export default SearchFeed; 
